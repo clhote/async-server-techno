79575d43def95b0fa91180ab7168301644ef4e91
Caro - <clhote@ece.fr>
2016-10-14 18:35:30 +0200

    tp2 : add script files

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var user = require ('./user.js');
 http.createServer(function (req, res) {
   user.get("Caroline", function(id){
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end ("Hello"+id);
+    res.end ("Hello "+id);
   })
 }).listen (1337, '127.0.0.1');
 
