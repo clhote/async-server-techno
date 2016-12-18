88e96f0e24ea2d787376337b5a36ec246de1d51e
gdesportes <gdesportes@bitbucket.org>
2016-12-18 18:47:06 +0100

    Amelioration du script start

--- bin/start.sh
+++ bin/start.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
+while true; do
+    read -p "Voulez vous remplir la base de données de test ?" yn
+    case $yn in
+        [Yy]* ) cd bin/; coffee metric.coffee; cd ..; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+cd bin/
+sh test.sh
+cd ..
 cd src/
 nodemon app.coffee
